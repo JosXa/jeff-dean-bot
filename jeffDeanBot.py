b8fdcf3e4625afe7df960bd2a2aa95c535b1e410
Joscha Götzer <joscha.goetzer@gmail.com>
2016-06-07 15:14:05 +0200

    sneak_case for telegram methods

--- jeffDeanBot.py
+++ jeffDeanBot.py
@@ -56,10 +56,10 @@ def main():
     dp = updater.dispatcher
 
     # Commands
-    dp.addHandler(CommandHandler("fact", sendFact))
-    dp.addHandler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("fact", sendFact))
+    dp.add_handler(CommandHandler("help", help))
 
-    dp.addErrorHandler(error)
+    dp.add_error_handler(error)
     updater.start_polling()
 
     print('Listening...')
