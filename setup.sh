2050071d532bac1bebd69b0fc31722076a76e88c
Joscha Götzer <joscha.goetzer@gmail.com>
2016-06-09 01:34:59 +0200

    getting install script to work...

--- setup.sh
+++ setup.sh
@@ -8,6 +8,4 @@ virtualenv venv
 source "$WORKSPACE/venv/bin/activate"
 # Install Requirements
 pip3 install -r requirements.txt
-# Run
-nohup ./venv/bin/python3 jeffDeanBot.py "207013186:AAHaGrj8R9Ii8rVaUeS0JIWm5aKtY_BTU0U" &>/dev/null 2>&1 &
-echo "Bot started and detached."
+
