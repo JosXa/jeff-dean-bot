3a1f3c1ec71b41719931b336cfaeb6d009b9d593
JosXa <joscha.goetzer@gmail.com>
2018-05-25 00:03:14 +0200

    commit this fucker

--- app.py
+++ app.py
@@ -105,15 +105,11 @@ def main():
     dp.add_handler(CommandHandler("help", send_help))
 
     dp.add_error_handler(error)
-    # updater.start_polling()
-    #
-    # print('Listening...')
-    # logging.info('Listening...')
-    #
-    # updater.idle()
-    port = int(environ.get('PORT', 5000))
-    updater.start_webhook(listen='0.0.0.0', port=port, url_path=token)
+    updater.start_polling()
+
+    print('Listening...')
     logging.info('Listening...')
+
     updater.idle()
 
 
